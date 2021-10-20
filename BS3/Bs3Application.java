d4467263e37fe6850b605870a4a55f755c41d1a2
IsraelBosonit <92779616+IsraelBosonit@users.noreply.github.com>
2021-10-20 13:59:56 +0200

    Add files via upload

--- BS3/Bs3Application.java
+++ BS3/Bs3Application.java
@@ -1,13 +1,29 @@
 package com.example.BS3;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Bs3Application {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bs3Application.class, args);
 	}
+	@Bean
+	CommandLineRunner ejecutate2(){
+		return p -> System.out.println("Hola desde la segunda clase");
+	}
+	@Bean
+	CommandLineRunner ejecutate3(){
+		return p -> System.out.println("Soy la tercera clase");
+	}
+	@PostConstruct
+	void ejecutate(){
+		System.out.println("Hola desde clase inicial");}
 
 }
