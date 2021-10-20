a94c261070d61c0877262f2da8b6f796549078d7
IsraelBosonit <92779616+IsraelBosonit@users.noreply.github.com>
2021-10-20 10:03:43 +0200

    Add files via upload

--- BS4/Ej2/Configuracion.java
+++ BS4/Ej2/Configuracion.java
@@ -1,9 +1,21 @@
 package com.example.BS4_2;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
+@Configuration
 @PropertySource("miconfiguracion.properties")
 public class Configuracion {
+    @Value("${valor1:no tiene valor}")
+    String valor1;
+    @Value("${valor2: no tiene valor}")
+    String valor2;
+    @Bean
+    CommandLineRunner ejecutame() {
+        return p -> { System.out.println("Valor1: "+valor1+","+"valor2: "+valor2); };
+    }
+    
 
 }
