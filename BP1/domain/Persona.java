7e727d065c50f65a6bca1f98d6c7d1ea3c4d9f80
IsraelBosonit <92779616+IsraelBosonit@users.noreply.github.com>
2021-10-26 09:12:09 +0200

    Add files via upload

---
+++ BP1/domain/Persona.java
@@ -0,0 +1,40 @@
+package com.example.DB1;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+    @Id
+    @GeneratedValue
+    private Integer id_persona;
+    @NotNull
+    private String usuario;
+    @NotNull
+    private String password;
+    @NotNull
+    private String name;
+    private String surname;
+    @NotNull
+    private String company_email;
+    @NotNull
+    private String personal_email;
+    @NotNull
+    private String city;
+    @NotNull
+    private Boolean active;
+    @NotNull
+    private Date created_date;
+    private String imagen_url;
+    private Date termination_date;
+
+}
